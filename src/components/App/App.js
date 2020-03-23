31d21d150b5c062903b7fa90db59e25680c6c813
Claire Olmstead <colmstead@lingoport.com>
2020-03-23 11:54:29 -0700

    getting rid of excess by cutting out boilerplate

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Footer from '../Footer/Footer';
+
+const App = ({ children }) => (
+  <>
+    <main>
+      {children}
+    </main>
+  </>
+);
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer id="footer">
+    <hr />
+    <p>Lingoport Inc.</p>
+  </footer>
+);
+
+export default Footer;
