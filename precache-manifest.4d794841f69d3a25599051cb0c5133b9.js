26de68ee049b1577869de7b07ae58b56e0199c41
Claire Olmstead <colmstead@lingoport.com>
2020-03-23 12:30:54 -0700

    Updates

---
+++ precache-manifest.4d794841f69d3a25599051cb0c5133b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83fbb4778b21f5e0010e1ebf676b48c",
+    "url": "/lrm_configure/index.html"
+  },
+  {
+    "revision": "577cee34bbee15f3d3a8",
+    "url": "/lrm_configure/static/css/main.7c670a56.chunk.css"
+  },
+  {
+    "revision": "295c2f4dc0c28d2e22cd",
+    "url": "/lrm_configure/static/js/2.764c1f91.chunk.js"
+  },
+  {
+    "revision": "7e45ed5b381a09128bbe006e7a079361",
+    "url": "/lrm_configure/static/js/2.764c1f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577cee34bbee15f3d3a8",
+    "url": "/lrm_configure/static/js/main.f4d2d9b0.chunk.js"
+  },
+  {
+    "revision": "fb37f8d655437d0726e9",
+    "url": "/lrm_configure/static/js/runtime-main.fc87ab24.js"
+  }
+]);
